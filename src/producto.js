1ac6a43e1daa903952b3cb154e8720ebd6794f1c
Nelfi Bombela <nelfibombelas123@gmail.com>
2026-02-05 20:25:29 -0400

    actualizacion de codigo 5

--- src/producto.js
+++
@@ -1,6 +0,0 @@
-export function crearProducto(nombre, precio) {
-  if (!nombre || precio <= 0) {
-    throw new Error("Datos inválidos");
-  }
-  return { nombre, precio };
-}
--- src/venta.js
+++
@@ -1,7 +0,0 @@
-export function calcularTotal(productos) {
-  if (productos.length === 0) {
-    return 0;
-  }
-
-  return productos.reduce((total, p) => total + p.precio, 0);
-}
